259d7f9fc2b3754499a0d666c3d503c6298f1c13
Mihály Kelemen <kelemenpc@hotmail.com>
2022-11-23 13:11:56 +0100

    Kezdő projekt

---
+++ src/main/java/beadando/f0202ea_erettsegiproject/HelloController.java
@@ -0,0 +1,14 @@
+package beadando.f0202ea_erettsegiproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module beadando.f0202ea_erettsegiproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens beadando.f0202ea_erettsegiproject to javafx.fxml;
+    exports beadando.f0202ea_erettsegiproject;
+}
