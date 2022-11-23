353e60203d62d2ebb203e69f68dedc86e8c8c973
evko01 <117721966+evko01@users.noreply.github.com>
2022-11-23 13:31:12 +0100

    Első feladat

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module beadando.f0202ea_erettsegiproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.hibernate.orm.core;
+    requires java.persistence;
+    requires java.naming;
+    requires java.sql;
 
     opens beadando.f0202ea_erettsegiproject to javafx.fxml;
     exports beadando.f0202ea_erettsegiproject;
